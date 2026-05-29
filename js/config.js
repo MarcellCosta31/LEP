f55c03f56201088be8b0502438b95aa64d9aae2a
Marcell Costa <marcellcostasena31@gmail.com>
2026-05-29 16:24:53 -0300

    3.4

---
+++ js/config.js
@@ -0,0 +1,14 @@
+// js/config.js - NÃO COMMITAR ESTE ARQUIVO!
+window.APP_CONFIG = {
+    // Firebase Config
+    FIREBASE_API_KEY: "AIzaSyBsDDnW7HZaie47AgjMaZ5U1orAiLvOaDM",
+    FIREBASE_AUTH_DOMAIN: "lep-reservas.firebaseapp.com",
+    FIREBASE_PROJECT_ID: "lep-reservas",
+    FIREBASE_STORAGE_BUCKET: "lep-reservas.firebasestorage.app",
+    FIREBASE_MESSAGING_SENDER_ID: "492338423428",
+    FIREBASE_APP_ID: "1:492338423428:web:7f72cdd8bcd4a5146f84d1",
+    
+    // Telegram Config
+    TELEGRAM_TOKEN: "8599499895:AAGWYnpH6UFm0m89WblXlQpgOtQZeAAuZwQ",
+    TELEGRAM_CHAT_ID: "-1003832202230"
+};
